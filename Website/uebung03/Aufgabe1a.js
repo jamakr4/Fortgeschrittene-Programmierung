7b5ceef4ea0b49a1a64b9ee82b1560fcb41e3d46
jamakr4 <68536642+jamakr4@users.noreply.github.com>
2024-04-25 23:51:41 +0200

    upload

---
+++ Website/uebung03/Aufgabe1a.js
@@ -0,0 +1,4 @@
+let x=0;
+if (x === false || x === 0 || x === -0 || x === "" || x === null || x === undefined || isNaN(x)) {
+  console.log("x hat einen der folgenden Werte: false, 0, -0, \"\", null, undefined oder NaN.");
+}
---
+++ Website/uebung03/Aufgabe1b.js
@@ -0,0 +1,14 @@
+let x = 1;
+let y = "1";
+
+if(x !== y)
+{
+    console.log("richtig1");
+} 
+
+if(x + 1 == y + 1)
+{
+    console.log("richtig2");
+} 
+
+console.log (y+1);
